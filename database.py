d7241fa9c261264342d1f36bc0711abdd4161247
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-12-27 20:35:14 -0300

    Correct field names in Movie and UserReview models

--- database.py
+++ database.py
@@ -15,13 +15,9 @@ class User(Model):
     password = CharField(max_length=50,)
     created_at = DateTimeField(default=datetime.now)
 
-    def __str__(self):
-        return self.username
-    
     class Meta:
         database = database
         table_name = "users"
-
 
     @classmethod
     def create_password(cls, password):
@@ -29,28 +25,31 @@ class User(Model):
         h.update(password.encode("utf-8"))
         return h.hexdigest()
 
+    def __str__(self):
+        return self.username
 
 class Movie (Model):
-    name = CharField(max_length=50)
+    title = CharField(max_length=50)
     created_at = DateTimeField(default=datetime.now)
 
-    def __str__(self):
-        return self.name
-    
     class Meta:
         database = database
         table_name = "movies"
 
+    def __str__(self):
+        return self.title
+    
 class UserReview(Model):
     user = ForeignKeyField(User, backref="reviews")
-    movie = ForeignKeyField(Movie, backref="reviews")
-    rating = IntegerField()
+    movie = ForeignKeyField(Movie)
     review = TextField()
+    score = IntegerField()
     created_at = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = database
+        table_name = "user_reviews" 
 
     def __str__(self):
         return f"{self.user.username} - {self.movie.name}"
     
-    class Meta:
-        database = database
-        table_name = "user_reviews" 
